3850c8d888fc4fbc490b55b4a62799af17778523
prajwaldankit <prajwal.dahal@ekbana.info>
2021-11-19 13:08:02 +0545

    Updated file to incorporate env file.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello World")
+import os
+import sys
+import pathlib
+from dotenv import load_dotenv
+
+load_dotenv()
+parent_dir = pathlib.Path(__file__).parent.resolve()
+sys.path.append(parent_dir)
+env = os.getenv("ENVIRONMENT")
+if not env or env == None:
+    env = "LOCAL"
+print(f'Program running in "{env}" mode.')
